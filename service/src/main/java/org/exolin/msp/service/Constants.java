dac7c310b007452920a97f4db45aa7b2aa51c923
tomgk <thomas.goelles@gmx.net>
2020-08-05 17:21:55 +0200

    Added comment

---
+++ service/src/main/java/org/exolin/msp/service/Constants.java
@@ -0,0 +1,12 @@
+package org.exolin.msp.service;
+
+/**
+ *
+ * @author tomgk
+ */
+public class Constants
+{
+    public static final String NAME = "org.exolin.msp:service-maven-plugin";
+    public static final String VERSION = "1.0";
+    public static final String ID = NAME + VERSION;
+}
