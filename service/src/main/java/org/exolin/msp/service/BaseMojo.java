cfd149811957ab4c32d269424a3d7036800ca205
tomgk <thomas.goelles@gmx.net>
2020-08-09 01:05:00 +0200

    Formatting

--- service/src/main/java/org/exolin/msp/service/BaseMojo.java
+++ service/src/main/java/org/exolin/msp/service/BaseMojo.java
@@ -11,28 +11,28 @@ import org.apache.maven.plugins.annotations.Parameter;
  */
 public abstract class BaseMojo extends AbstractMojo
 {
-    @Parameter( defaultValue = "${project.packaging}", property = "packaging", required = true )
+    @Parameter( defaultValue = "${project.packaging}", property = "packaging", required = true)
     protected String packaging;
     
-    @Parameter(defaultValue = "${project.build.directory}/${project.build.finalName}.${project.packaging}", property = "jar", required = true )
+    @Parameter(defaultValue = "${project.build.directory}/${project.build.finalName}.${project.packaging}", property = "jar", required = true)
     protected File jar;
     
-    @Parameter(defaultValue = "${project.build.directory}", required = true )
+    @Parameter(defaultValue = "${project.build.directory}", required = true)
     protected File targetDir;
     
-    @Parameter(defaultValue = "${project.build.directory}/lib", property = "libDir", required = true )
+    @Parameter(defaultValue = "${project.build.directory}/lib", property = "libDir", required = true)
     protected File libDir;
     
-    @Parameter(defaultValue = "${project.build.directory}/service-deployment", property = "service-deployment-temp", required = true )
+    @Parameter(defaultValue = "${project.build.directory}/service-deployment", property = "service-deployment-temp", required = true)
     protected File pluginDir;
     
-    @Parameter(defaultValue = "${service.user}", property = "serviceUser", required = true )
+    @Parameter(defaultValue = "${service.user}", property = "serviceUser", required = true)
     protected String serviceUser;
     
     @Parameter(defaultValue = "${project.artifactId}", property = "serviceName", required = true)
     protected String serviceName;
     
-    @Parameter(defaultValue = "${project.description}", property = "serviceDescription", required = true )
+    @Parameter(defaultValue = "${project.description}", property = "serviceDescription", required = true)
     protected String serviceDescription;
     
     protected File pluginDir() throws IOException
