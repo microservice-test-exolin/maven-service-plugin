7f95c60f80c026eafe993c4969e5f82704a75051
tomgk <thomas.goelles@gmx.net>
2020-08-23 00:47:10 +0200

    Minor Layout modifications

--- web-ui/src/main/java/org/exolin/msp/web/ui/servlet/Layout.java
+++ web-ui/src/main/java/org/exolin/msp/web/ui/servlet/Layout.java
@@ -3,6 +3,7 @@ package org.exolin.msp.web.ui.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
+import org.exolin.msp.web.ui.Constants;
 import org.exolin.msp.web.ui.Main;
 
 /**
@@ -61,13 +62,13 @@ public class Layout
         w.append("<ul class=\"nav flex-column\">");
         
         writeMenuItem(w, "Dashboard", "/", "home", current.equals("/"));
-        writeMenuItem(w, "Services", "/services", SERVICE, current.startsWith("/services"));
+        writeMenuItem(w, "Services", "/services", SERVICE, current.startsWith("/service"));
         writeMenuItem(w, "Processes", "/processes", PROCESS, current.startsWith("/processes"));
         writeMenuItem(w, "Logs", "/logs", LOG, current.startsWith("/logs"));
         writeMenuItem(w, "Server Info", "/server-info", "server", current.startsWith("/server-info"));
         
         w.write("</ul>");
-        w.write("<div class=\"footer\">Started at "+Main.startedAt+"</div>");
+        w.write("<div class=\"footer\">Started at "+Main.startedAt+"<br>Version:"+Constants.VERSION+"</div>");
         w.write("</div>");
         w.write("</nav>");
     }
